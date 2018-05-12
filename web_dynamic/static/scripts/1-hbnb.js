0454ca4305093f5c398f5deea51cd17f55e45eb9
Dimitrios Philliou <dimitrios@Dimitrioss-MacBook-Air.local>
2018-05-11 18:52:49 -0700

    Made checkboxes work. Had to change render_template in .py file.

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,11 @@
-$(document) () => {
-  let checks = {};
-  $('input[type=checkbox]').click(
-    function (){
-      if (this.checked) {
-        let id = this.dataset.id;
-        checks[id] = this.dataset.name;
-      } else {
-        delete checks[this.dataset.id];
-      }
-    });
-  $('.amenities h4').text(Object.values(checks))
-}
+let checks = {};
+$(() => {
+  $('input[type=checkbox]').click(function () {
+    if (this.checked) {
+      checks[this.dataset.id] = this.dataset.name;
+    } else {
+      delete checks[this.dataset.id];
+    }
+    $('.amenities h4').text(Object.values(checks).join(', '));
+  });
+});
