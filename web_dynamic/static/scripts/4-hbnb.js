1137fb7ad047dda38ec6b0a51016cd13a78c7f56
Jacob Biehler <biehlerj@gmail.com>
2018-05-14 18:02:29 -0700

    Putting everything in the document ready

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,5 +1,43 @@
 let checks = {};
 $(() => {
+  function postPlaces (data) {
+    data.sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+    data.forEach(function (place) {
+      let info = ` 
+      <article>
+      <div class="title">
+      <h2>${place.name}</h2>
+      <div class="price_by_night">
+      $${place.price_by_night}
+      </div>
+      </div>
+      <div class="information">
+      <div class="max_guest">
+      <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+      <br />
+      ${place.max_guest} Guests
+      </div>
+      <div class="number_rooms">
+      <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+      <br />
+      ${place.number_rooms} Bedrooms
+      </div>
+      <div class="number_bathrooms">
+      <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+      <br />
+      ${place.number_bathrooms} Bathroom
+      </div>
+      </div>
+      <div class="description">
+      ${place.description}
+      </div>
+      </article>
+      `;
+      $('section.places').append(info);
+    });
+  }
   $.get(('http://0.0.0.0:5001/api/v1/status/'), function (data) {
     if (data.status === 'OK') {
       $('#api_status').addClass('available');
@@ -14,66 +52,25 @@ $(() => {
     }
     $('.amenities h4').text(Object.values(checks).join(', '));
   });
-});
-
-$(() => $.ajax({
-  type: 'POST',
-  url: 'http://0.0.0.0:5001/api/v1/places_search/',
-  contentType: 'application/json',
-  data: '{}',
-  dataType: 'json',
-  success: postPlaces
-}));
-
-$('#button').click(function (event) {
-  let checkedObjs = {};
-  console.log('CLICK SUCCESSFUL!');
-  checkedObjs.amenities = Object.keys(checks);
+  $('#button').click(function () {
+    let checkedObjs = {};
+    console.log('CLICK SUCCESSFUL!');
+    checkedObjs.amenities = Object.keys(checks);
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      contentType: 'application/json',
+      data: JSON.stringify(checkedObjs),
+      dataType: 'json'
+    })
+      .done(postPlaces);
+  });
   $.ajax({
     type: 'POST',
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
     contentType: 'application/json',
-    data: checkedObjs,
+    data: '{}',
     dataType: 'json',
-    error: console.log('ERROR')
-  })
-    .done(postPlaces);
+    success: postPlaces
+  });
 });
-function postPlaces (data) {
-  data.sort((a, b) => {
-    return a.name.localeCompare(b.name);
-  });
-  data.forEach(function (place) {
-    let info = ` 
-    <article>
-    <div class="title">
-    <h2>${place.name}</h2>
-    <div class="price_by_night">
-    ${place.price_by_night}
-    </div>
-    </div>
-    <div class="information">
-    <div class="max_guest">
-    <i class="fa fa-users fa-3x" aria-hidden="true"></i>
-    <br />
-    ${place.max_guest} Guests
-    </div>
-    <div class="number_rooms">
-    <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
-    <br />
-    ${place.number_rooms} Bedrooms
-    </div>
-    <div class="number_bathrooms">
-    <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
-    <br />
-    ${place.number_bathrooms} Bathroom
-    </div>
-    </div>
-    <div class="description">
-    ${place.description}
-    </div>
-    </article>
-    `;
-    $('section.places').append(info);
-  });
-}
